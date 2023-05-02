6b2de74c8b00dd8a15d2a97e59ca2ac103d6080d
Fsxerath <frediangarita1999@gmail.com>
2023-05-02 16:41:37 -0500

    Se crea pantalla del login

---
+++ app/src/main/java/com/example/jacket_tracker/Login_view.kt
@@ -0,0 +1,11 @@
+package com.example.jacket_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_view)
+    }
+}
