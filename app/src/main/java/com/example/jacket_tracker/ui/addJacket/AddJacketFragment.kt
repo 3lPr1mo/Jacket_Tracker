4faf2d2fdb7aeb7d36ef1fc78a73c853fecb5c3c
ErickCh8023 <eechaparrom@correo.udistrital.edu.co>
2023-05-02 17:21:11 -0500

    Tooblar complete, fragments left

---
+++ app/src/main/java/com/example/jacket_tracker/ui/addJacket/AddJacketFragment.kt
@@ -0,0 +1,42 @@
+package com.example.jacket_tracker.ui.addJacket
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.jacket_tracker.databinding.FragmentNotificationsBinding
+
+class AddJacketFragment : Fragment() {
+
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val addJacketViewModel =
+            ViewModelProvider(this).get(AddJacketViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        addJacketViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/jacket_tracker/ui/addJacket/AddJacketViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.jacket_tracker.ui.addJacket
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddJacketViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
