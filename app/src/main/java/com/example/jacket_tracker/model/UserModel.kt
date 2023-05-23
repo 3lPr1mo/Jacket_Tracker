d6a0e1d8b580f9649cd38eb4554d1cfff3e89122
3lPr1mo <erickenriquechaparro@gmail.com>
2023-05-23 13:46:37 -0500

    Added Firebase dependencies

---
+++ app/src/main/java/com/example/jacket_tracker/model/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.jacket_tracker.model
+
+data class UserModel(
+    var firstname: String,
+    var lastname: String,
+    var id: Int,
+    var password: String,
+    var email: String
+    )
